87bcb7274ceb68267378b060020da0aa93466a33
tandukar <84461389+tandukar@users.noreply.github.com>
2023-07-26 21:14:02 +0545

    sidebar

--- client/src/components/pages/main/Sidebar.tsx
+++ client/src/components/pages/main/Sidebar.tsx
@@ -2,6 +2,15 @@ import Nav from "./Nav";
 import { FaDiscord } from "react-icons/fa";
 
 const Sidebar = () => {
+  const groupChatData = [
+    { id: 1, imageUrl: "https://pbs.twimg.com/profile_images/851446117216727040/CkSeULeI_400x400.jpg" },
+    { id: 2, imageUrl: "https://picsum.photos/seed/picsum/200/300" },
+    { id: 4, imageUrl: "https://picsum.photos/200/300?grayscale" },
+    { id: 5, imageUrl: "https://picsum.photos/200/300/?blur" },
+    
+    
+  ];
+
   return (
     <div>
       <div className="flex flex-row h-screen">
@@ -9,15 +18,19 @@ const Sidebar = () => {
           <div className="flex justify-center items-center h-11 w-11 rounded-2xl bg-dc1 mb-5">
             <FaDiscord className="h-7 w-7 text-neutral-200" />
           </div>
+
+          {groupChatData.map((data) => (
           <div className=" mb-4">
             <div className="h-11 w-11 rounded-3xl overflow-hidden transition-all duration-300 hover:rounded-2xl">
               <img
-                src="https://pbs.twimg.com/profile_images/851446117216727040/CkSeULeI_400x400.jpg"
+                src={data.imageUrl}
                 alt="Profile"
+                key={data.id}
                 className="h-full w-full object-cover"
               />
             </div>
-          </div>
+            </div>
+            ))}
         </div>
       </div>
     </div>
